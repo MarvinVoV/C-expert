732e569880cce5af4b3d24445c746afcd31c005c
yamorn <yamorn@outlook.com>
2015-04-10 13:37:13 +0800

    add pipe line

--- test.c
+++ test.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-    char str[1024]; 
-    sprintf(str,"hello world\n");
-    printf("%*s%s\n",4,"",str);
+	printf("BUFSIZ=%d\n",BUFSIZ);
     exit(0);
 }
