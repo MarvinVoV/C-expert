6c1ad4e10536b46210f3653b29ab223ffaa811a0
yamorn <yamorn@outlook.com>
2015-04-30 17:32:51 +0800

    add network

---
+++ socket/error.c
@@ -0,0 +1,20 @@
+#include "netcomplex.h"
+#include <stdarg.h>
+
+void err_sys(const char *fmt, ...){
+	va_list ap;
+	err_print(fmt,ap);
+	exit(1);
+}
+
+void err_msg(const char *fmt, ...){
+	va_list ap;
+	err_print(fmt,ap);
+	return;
+}
+
+void err_print(const char *fmt,va_list ap){
+	va_start(ap);
+	vprintf(fm,ap);
+	va_end(ap);
+}
---
+++ socket/netcomplex.h
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/signal.h>
+#include <errno.h>
+
+
