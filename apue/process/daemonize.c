5c33440b91ea81bde64282806819f4a21cf264d2
yamorn <yamorn@outlook.com>
2016-05-04 17:55:05 +0800

    daemon

--- apue/process/daemonize.c
+++ apue/process/daemonize.c
@@ -8,26 +8,49 @@
 
 void daemonize(const char *cmd){
     int i, fd0, fd1, fd2;
-    pid_t pid;
+    pid_t pid, sid;
     struct rlimit rl;
     struct sigaction sa;
 
     /* Clear file creation mask */
     umask(0);
 
-    /* Get maximum number of file descriptors. */
+	/* fork off the parent process */
+	pid = fork();
+	if(pid < 0){
+		perror("fork error");
+		exit(EXIT_FAILURE);
+	}else if(pid != 0){ /* parent */
+		exit(EXIT_SUCCESS);
+	}
+	
+	/* Create a new SID and become a session leader to lose controlling TTY.*/
+	sid = setsid();
+	if(sid < 0){
+		perror("setsid error");
+		exit(EXIT_FAILURE);
+	}
+
+    /*
+     * Change the current working directory to the root so 
+     * we won't prevent file systems from being unmounted.
+     */
+    if(chdir("/") < 0){
+        perror("can't change directory to /");
+		exit(EXIT_FAILURE);
+    }
+	
+
+    /* Get maximum number of file descriptors. and close all open file descriptors */
     if(getrlimit(RLIMIT_NOFILE, &rl) < 0){
         perror("can't get file limit");
         exit(-1);
     }
+    if(rl.rlim_max == RLIM_INFINITY)
+        rl.rlim_max = 1024;
+    for(i = 0; i < rl.rlim_max; i++)
+        close(i);
 
-    /* Become a session leader to lose controlling TTY. */
-    if((pid = fork()) < 0){
-        perror("fork error");
-        exit(-1);
-    }else if(pid != 0) /* parent */
-        exit(0);
-    setsid();
 
     /* Ensure future opens won't allocate controlling TTYs. */
     sa.sa_handler = SIG_IGN;
@@ -37,28 +60,7 @@ void daemonize(const char *cmd){
         perror("can't ignore SIGHUP");
         exit(-1);
     }
-    if((pid = fork()) < 0){
-        perror("fork error");
-        exit(-1);
-    }else if(pid != 0) /* parent */
-        exit(0);
 
-    /*
-     * Change the current working directory to the root so 
-     * we won't prevent file systems from being unmounted.
-     */
-    if(chdir("/") < 0){
-        perror("can't change directory to /");
-        exit(-1);
-    }
-
-    /*
-     * Close all open file descriptors.
-     */
-    if(rl.rlim_max == RLIM_INFINITY)
-        rl.rlim_max = 1024;
-    for(i = 0; i < rl.rlim_max; i++)
-        close(i);
 
     /* Attach file descriptors 0, 1, 2 to /dev/null. */
     fd0 = open("/dev/null", O_RDWR);
