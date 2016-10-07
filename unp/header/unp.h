f99e94221e9423568ecf9f381bb954faff362e86
yamorn sun <yamorn@192.168.2.102>
2016-10-07 23:59:00 +0800

    add unp

---
+++ unp/header/unp.h
@@ -0,0 +1,31 @@
+/*
+ * unp.h
+ *
+ *  Created on: Oct 7, 2016
+ *      Author: yamorn
+ */
+
+#ifndef HEADER_UNP_H_
+#define HEADER_UNP_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define SERV_PORT 8080
+#define BUF_SIZE  2048 * 10
+
+
+ssize_t readn(int fd, void *buf, size_t nbytes);
+
+ssize_t writen(int fd, const void *buf, size_t nbytes);
+
+ssize_t readline(int fd, void *buf, size_t maxlen);
+
+#endif /* HEADER_UNP_H_ */
