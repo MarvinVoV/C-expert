3b2b2deb162e5c0b0937781e47d686fa5a72ff56
yamorn sun <yamorn@outlook.com>
2016-10-26 00:19:29 +0800

    front

--- unp/routines/echo/echoserver.c
+++ unp/routines/echo/echoserver.c
@@ -64,7 +64,8 @@ again:
 void sigchld_handler(int signo){
     pid_t pid;
     int stat;
-    pid = wait(&stat);
-    printf("child %d terminated \n", pid);
+
+    while ((pid = waitpid(-1, &stat, WNOHANG) > 0)
+        printf("child %d terminated \n", pid);
     return;
 }
