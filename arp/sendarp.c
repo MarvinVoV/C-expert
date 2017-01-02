35d88a1850704d3afdaa593094fd796c1d9a94c5
yamorn <yamorn@outlook.com>
2017-01-02 22:26:48 +0800

    modify

--- arp/sendarp.c
+++ arp/sendarp.c
@@ -3,12 +3,15 @@
 #include <string.h>				// strcpy(), memset(), memcpy()
 #include <unistd.h>				// close()
 #include <stdint.h>				// uint8_t, uint16_6
-#include <sys/socket.h>			// socket()
-#include <netinet/if_ether.h>	// ETH_P_ARP = 0x0806
+#include <netinet/if_ether.h>	// ETH_P_ARP, ARPOP_REQUEST, ETH_ALEN, ETH_HLEN
 #include <netinet/in.h>			// IPPROTO_RAW, INET_ADDRSTRLEN
 #include <netinet/ip.h>			// IP_MAXPACKET (which is 65535)
-#include <net/if.h>				// struct ifreq, IFNAMSIZ
 #include <sys/ioctl.h>			// ioctl
+#include <sys/socket.h>			// socket()
+#include <net/ethernet.h>		// ETHERTYPE_IP, struct ether_header
+#include <net/if.h>				// struct ifreq, IFNAMSIZ, if_nametoindex()
+#include <net/if_arp.h>			// ARPHRD_ETHER
+#include <netpacket/packet.h>	// struct sockaddr_ll
 #include <errno.h>				// error, perror()
 
 
@@ -21,16 +24,80 @@ int main(int argc, char **argv) {
 
 	char *ifname = "ens33";
 
+	char src_ip[INET_ADDRSTRLEN];
+
+	char dst_ip[INET_ADDRSTRLEN];
+
 	struct ether_arp arp;
 
-	int sockfd;
+	struct ether_header etherhdr;
+
+	struct sockaddr_ll device;
+
+	int sockfd, frame_len;
 
 	uint8_t src_mac[6], dst_mac[6];
 
+	uint8_t ether_frame[IP_MAXPACKET];
+
+
+
+	// Set source IP
+	strcpy(src_ip, "172.16.200.128");
+
+	// Set target IP
+	strcpy(dst_ip, "192.168.3.1");
+
+	// Set source mac address
 	getmacaddr(ifname, src_mac);
 
-	printmacaddr(src_mac);
+	// Set destination mac address
+	memset(dst_mac, 0xff, sizeof(dst_mac));
 
+	// Set ARP Packet
+	arp.arp_hrd = htons(ARPHRD_ETHER);
+	arp.arp_pro = htons(ETHERTYPE_IP);
+	arp.arp_hln = 6;
+	arp.arp_pln = 4;
+	arp.arp_op = htons(ARPOP_REQUEST);
+	memcpy(arp.arp_sha, src_mac, ETH_ALEN * sizeof(uint8_t));
+	memcpy(arp.arp_tha, dst_mac, ETH_ALEN * sizeof(uint8_t));
+	memcpy(arp.arp_spa, src_ip, 4 * sizeof(uint8_t));
+	memcpy(arp.arp_tpa, dst_ip, 4 * sizeof(uint8_t));
+
+	// Set Ethernet header
+	memcpy(etherhdr.ether_shost, src_mac, ETH_ALEN * sizeof(uint8_t));
+	memcpy(etherhdr.ether_dhost, dst_mac, ETH_ALEN * sizeof(uint8_t));
+	etherhdr.ether_type = ETHERTYPE_ARP;
+
+	// Package
+	memcpy(ether_frame, etherhdr, ETH_HLEN);
+	memcpy(ether_frame + ETH_HLEN, &arp, uint8_t * sizeof(arp));
+
+	frame_len = ETH_HLEN + uint8_t * sizeof(arp);
+
+	sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+	if (sockfd < 0) {
+		perror("socket error.");
+		exit(EXIT_FAILURE);
+	}
+
+	memset (&device, 0, sizeof (device));
+  	device.sll_family = AF_PACKET;
+  	if ((device.sll_ifindex = if_nametoindex (interface)) == 0) {
+    	perror ("if_nametoindex() failed to obtain interface index ");
+    	exit (EXIT_FAILURE);
+  	}
+  	memcpy (device.sll_addr, src_mac, ETH_ALEN * sizeof (uint8_t));
+  	device.sll_halen = ETH_ALEN;
+
+  	if ((bytes = sendto (sockfd, ether_frame, frame_len, 0, (struct sockaddr *) &device, sizeof(device))) <= 0) {
+    	perror ("sendto() failed");
+    	exit (EXIT_FAILURE);
+  	}
+
+  	close(sockfd);
+  	exit(EXIT_SUCCESS);
 }
 
 /**
@@ -53,8 +120,6 @@ void getmacaddr(char *iface, uint8_t *macaddr) {
 	close(fd);
 
 	mac = (uint8_t *) ifr.ifr_hwaddr.sa_data;
-
-	printmacaddr(mac);
 
 	memcpy(macaddr, mac, sizeof(mac));
 }
