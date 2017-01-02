2806d5684918ad2db41509e89d7b4f7b5e4a3182
yamorn <yamorn@outlook.com>
2017-01-02 19:46:37 +0800

    test

--- arp/sendarp.c
+++ arp/sendarp.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>				// strcpy(), memset(), memcpy()
-#include <unistd.h>
+#include <unistd.h>				// close()
 #include <stdint.h>				// uint8_t, uint16_6
 #include <sys/socket.h>			// socket()
 #include <netinet/if_ether.h>	// ETH_P_ARP = 0x0806
 #include <netinet/in.h>			// IPPROTO_RAW, INET_ADDRSTRLEN
 #include <netinet/ip.h>			// IP_MAXPACKET (which is 65535)
-#include <ifaddrs.h>			// struct ifaddrs
+#include <net/if.h>				// struct ifreq, IFNAMSIZ
+#include <sys/ioctl.h>			// ioctl
 #include <error.h>				// error, perror()
 
 
-#define INTERFACE "ens33"
+uint8_t *getmacaddr(char *);
+
 
 int main(int argc, char **argv) {
 
@@ -30,17 +32,24 @@ int main(int argc, char **argv) {
 }
 
 
-void getmacaddr(char *interface, uint8_t *macaddr) {
-	struct ifaddr *iflist;
-	if (getifaddrs(&iflist) == 0) {
-		for (struct ifaddr *cur = iflist; cur; cur = cur->ifa_next) {
-			if (cur->ifa_addr->sa_family == AF_LINK && cur->ifa_addr) {
-				//struct sockaddr_dl* sdl = (struct sockaddr_dl*)cur->ifa_addr;
-				//uint8_t *ptr = (uint8_t *)LLADDR(sdl);
-				printf("interface = %s\n", cur->ifa_name);
-			}
-		}
-	}
+uint8_t *getmacaddr(char *iface) {
+	int fd;
+	struct ifreq ifr;
+
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	ifr.ifr_addr.sa_family = AF_INET;
+	strncpy(ifr.ifr_name, iface, IFNAMSIZ - 1);
+
+	ioctl(fd, SIOCGIFHWADDR, &ifr);
+
+	close(fd);
+
+	macaddr = (uint8_t *) ifr.ifr_hwaddr.sa_data;
+
+	printf("Mac : %.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n" , mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+	return macaddr;
 }
 
 
